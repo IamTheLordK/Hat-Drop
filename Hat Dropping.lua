7e0ab51f1ce10b9f269d3444c32b67c8c2607f25
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2025-12-19 22:18:03 -0300

    Update Hat Dropping.lua

--- Hat Dropping.lua
+++ Hat Dropping.lua
@@ -4,9 +4,14 @@ local Hats = Char.Humanoid:GetAccessories()
 
 local D = workspace.FallenPartsDestroyHeight
 
+local Old = Instance.new("Part", workspace)
+Old.Size = Vector3.one * 0
+Old.Anchored = true
+Old.CanCollide = false
+Old.CFrame = Root.CFrame
+
 for _, v in pairs(Hats) do if v:GetChildren()[1]:GetJoints()[1].Part1 == Char.Head then sethiddenproperty(v, "BackendAccoutrementState", 2) end end
 
-game.Players.LocalPlayer.SimulationRadius = 999
 workspace.FallenPartsDestroyHeight = 0/0
 
 local A = Instance.new("Animation")
@@ -19,22 +24,37 @@ spawn(function()
  while Root.Parent do
   Root.CFrame = CFrame.new(Root.Position.X, Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and D - 0.7 or D - 4, Root.Position.Z)
   Root.Velocity = Vector3.new(0, 26, 0)
+  game.Players.LocalPlayer.SimulationRadius = 999
   game:GetService("RunService").PostSimulation:Wait()
  end
 end)
 
-wait(.45) Char.Humanoid:ChangeState(15) wait(.1)
+task.wait(.45) 
+Char.Humanoid:ChangeState(15)
+Char.ChildRemoved:Wait()
 
 local Success = false
-repeat for _, v in pairs(Hats) do local H = v:FindFirstChild("Handle") sethiddenproperty(v, "BackendAccoutrementState", 2) if H then H.AssemblyLinearVelocity = Vector3.new(0, 30, 0) if H and H.CanCollide then Success = true break end end end if game.Players.LocalPlayer.Character ~= Char then break end task.wait() until Success
+
+repeat
+ for _, v in pairs(Hats) do
+  local H = v:FindFirstChild("Handle")
+  sethiddenproperty(v, "BackendAccoutrementState", 2) if H then
+   H.AssemblyLinearVelocity = Vector3.new(0, 30, 0)
+   if H.CanCollide then Success = true break end
+  end
+ end
+ 
+ if game.Players.LocalPlayer.Character ~= Char then break end
+ task.wait()
+until Success
 
 workspace.FallenPartsDestroyHeight = D
 
 if not Success then print("failed") else warn("sucess")
  for _, v in pairs(Hats) do
   local H = v:FindFirstChild("Handle") if H then
-   workspace.CurrentCamera.CameraSubject = workspace.T3
-   H.CFrame = workspace.T3.CFrame
+   workspace.CurrentCamera.CameraSubject = Old
+   H.CFrame = Old.CFrame
   end
  end
 end
