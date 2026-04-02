3eb101bd252cf72512a0f76f171e1f7aca9a46f4
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-02 20:15:09 -0300

    Refactor hat dropping logic and improve readability

--- Hat Dropping.lua
+++ Hat Dropping.lua
@@ -1,16 +1,16 @@
-local Char = game.Players.LocalPlayer.Character
-local Root = Char:WaitForChild("HumanoidRootPart")
-local Hats = Char.Humanoid:GetAccessories()
+local Char = game:GetService("Players").LocalPlayer.Character
 
+local Root = Char.HumanoidRootPart
+local Hats = Char.Humanoid:GetAccessories()
 local D = workspace.FallenPartsDestroyHeight
 
 local Old = Instance.new("Part", workspace)
-Old.Size = Vector3.one * 0
+Old.Size = Vector3.zero
 Old.Anchored = true
 Old.CanCollide = false
 Old.CFrame = Root.CFrame
 
-for _, v in pairs(Hats) do if v:GetChildren()[1]:GetJoints()[1].Part1 == Char.Head then sethiddenproperty(v, "BackendAccoutrementState", 2) end end
+for _, V in Hats do if V:GetChildren()[1]:GetJoints()[1].Part1 == Char.Head then sethiddenproperty(V, "BackendAccoutrementState", 2) end end
 
 workspace.FallenPartsDestroyHeight = 0/0
 
@@ -18,43 +18,41 @@ local A = Instance.new("Animation")
 A.AnimationId = "rbxassetid://" .. (Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and 507767968 or 220512718)
 local Track = Char.Humanoid:LoadAnimation(A)
 Track:Play(0, 1, 0)
-Track.TimePosition = Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and .1 or .72
+Track.TimePosition = Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and 0.1 or 0.72
 
-spawn(function()
- while Root.Parent do
-  Root.CFrame = CFrame.new(Root.Position.X, Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and D - 0.7 or D - 4, Root.Position.Z)
-  Root.Velocity = Vector3.new(0, 26, 0)
-  game.Players.LocalPlayer.SimulationRadius = 999
-  game:GetService("RunService").PostSimulation:Wait()
- end
+task.spawn(function()
+while Root.Parent do game:GetService("RunService").PostSimulation:Wait()
+ Root.CFrame = CFrame.new(Root.Position.X, Char.Humanoid.RigType == Enum.HumanoidRigType.R15 and D - 0.7 or D - 4, Root.Position.Z)
+ Root.Velocity = Vector3.new(0, 26, 0)
+ game:GetService("Players").LocalPlayer.SimulationRadius = 999
+end
 end)
 
-task.wait(.45) 
+task.wait(.45)
 Char.Humanoid:ChangeState(15)
 Char.ChildRemoved:Wait()
 
 local Success = false
 
 repeat
- for _, v in pairs(Hats) do
-  local H = v:FindFirstChild("Handle")
-  sethiddenproperty(v, "BackendAccoutrementState", 2) if H then
-   H.AssemblyLinearVelocity = Vector3.new(0, 30, 0)
-   if H.CanCollide then Success = true break end
-  end
+for _, V in Hats do
+ local H = V:FindFirstChild("Handle")
+ sethiddenproperty(V, "BackendAccoutrementState", 2) if H then
+  H.AssemblyLinearVelocity = Vector3.new(0, 30, 0)
+  if H.CanCollide then Success = true break end
  end
- 
- if game.Players.LocalPlayer.Character ~= Char then break end
- task.wait()
+end
+if game:GetService("Players").LocalPlayer.Character ~= Char then break end
+task.wait()
 until Success
 
 workspace.FallenPartsDestroyHeight = D
 
-if not Success then print("failed") else warn("sucess")
- for _, v in pairs(Hats) do
-  local H = v:FindFirstChild("Handle") if H then
-   workspace.CurrentCamera.CameraSubject = Old
-   H.CFrame = Old.CFrame
-  end
+if not Success then print("failed") else warn("success")
+for _, V in Hats do
+ local H = V:FindFirstChild("Handle") if H then
+  workspace.CurrentCamera.CameraSubject = Old
+  H.CFrame = Old.CFrame
  end
+end
 end
